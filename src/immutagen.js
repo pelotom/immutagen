57561fdcd4b2e0ede70df213ffb830d24fa94da7
Tom Crockett <pelotom@gmail.com>
2016-05-21 15:19:05 -0700

    Rename 'genFactory' to 'genFun'

--- src/immutagen.js
+++ src/immutagen.js
@@ -1,11 +1,11 @@
 // A simple immutable generator simulator that replays history in order to
 // "clone" JavaScript's mutable generators
 
-export default genFactory => {
+export default genFun => {
   const nextFor = history => input => {
     const
       newHist = history.concat([input]),
-      gen = genFactory(newHist[0]),
+      gen = genFun(newHist[0]),
       { value, done } = newHist.map(x => gen.next(x))[history.length]
 
     return {
